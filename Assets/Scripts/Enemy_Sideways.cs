02b742e6e430f4a18761a28dd3859d26a7ca02d3
AntoniaEma <antonia.tufanescu@student.upt.ro>
2025-01-10 22:11:38 +0200

    Moving Enemies

--- Assets/Scripts/Enemy_Sideways.cs
+++ Assets/Scripts/Enemy_Sideways.cs
@@ -2,7 +2,40 @@ using UnityEngine;
 
 public class Enemy_Sideways : MonoBehaviour
 {
+    [SerializeField] private float movementDistance;
+    [SerializeField] private float Speed;
     [SerializeField] private float damage;
+    private bool movingLeft;
+    private float leftEdge;
+    private float rightEdge;
+
+    private void Awake()
+    {
+        leftEdge = transform.position.x - movementDistance;
+        rightEdge = transform.position.x + movementDistance;
+    }
+
+    private void Update()
+    {
+        if (movingLeft)
+        {
+            if (transform.position.x > leftEdge)
+            {
+                transform.position = new Vector3(transform.position.x - Speed * Time.deltaTime, transform.position.y, transform.position.z);
+            }
+            else
+                movingLeft = false;
+        }
+        else
+        {
+            if (transform.position.x < rightEdge)
+            {
+                transform.position = new Vector3(transform.position.x + Speed * Time.deltaTime, transform.position.y, transform.position.z);
+            }
+            else
+                movingLeft = true;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
